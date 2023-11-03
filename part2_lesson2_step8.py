0e1eaaadef23ab05b565cd650986fa654299bc40
StRavenwing <lipatova_2013@inbox.ru>
2023-11-03 11:43:29 +0300

    add 3 new lessons

---
+++ part2_lesson2_step8.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+
+try: 
+    link = "http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    #labels = browser.find_elements_by_tag_name('label') # Список лэйблов над текстовыми полями
+    #inputs = browser.find_elements_by_tag_name('input') # Список текстовых полей
+
+    input1 = browser.find_element(By.NAME, "firstname")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "lastname")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.NAME, "email")
+    input3.send_keys("test@email.com")
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    file_name = "file.txt"
+    file_path = os.path.join(current_dir, file_name)
+    element = browser.find_element(By.ID, "file")
+    element.send_keys(file_path)
+    
+  
+    button = browser.find_element(By.CLASS_NAME, "btn.btn-primary")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
