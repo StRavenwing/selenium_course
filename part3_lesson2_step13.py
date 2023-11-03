96307252fe850c8d2dfc355f57e192928b3c2d77
StRavenwing <lipatova_2013@inbox.ru>
2023-11-03 19:26:16 +0300

    Fix bug in part2_lesson2.step13.py

--- part3_lesson2_step13.py
+++ part3_lesson2_step13.py
@@ -2,26 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import unittest
+#import pytest
 
 class TestReg(unittest.TestCase):
 
     def test_reg1(self):
-        try: 
-            link = "http://suninjuly.github.io/registration2.html"
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
             browser = webdriver.Chrome()
             browser.get(link)
 
-            required_elements = browser.find_elements(By.CSS_SELECTOR, ":required") #list of required fields 
-            for element in required_elements:
-                element.send_keys('required') # Список текстовых полей
-
+            input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
+            input1.send_keys("Ivan")
+            input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')
+            input2.send_keys("Petrov")
+            input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')
+            input3.send_keys("test@email.com")
             # Отправляем заполненную форму
             button = browser.find_element(By.CSS_SELECTOR, "button.btn")
             button.click()
 
             # Проверяем, что смогли зарегистрироваться
             # ждем загрузки страницы
-            time.sleep(10)
+            
 
             # находим элемент, содержащий текст
             welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
@@ -39,16 +42,20 @@ class TestReg(unittest.TestCase):
             browser = webdriver.Chrome()
             browser.get(link)
 
-            required_elements = browser.find_elements(By.CSS_SELECTOR, ":required") #list of required fields 
-            for element in required_elements:
-                element.send_keys('required') # Список текстовых полей
+            input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
+            input1.send_keys("Ivan")
+            input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')
+            input2.send_keys("Petrov")
+            input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')
+            input3.send_keys("test@email.com")
+            # Отправляем заполненную форму
 
             button = browser.find_element(By.CSS_SELECTOR, "button.btn")
             button.click()
 
             # Проверяем, что смогли зарегистрироваться
             # ждем загрузки страницы
-            time.sleep(10)
+          
 
             # находим элемент, содержащий текст
             welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
